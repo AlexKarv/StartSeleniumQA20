99dae57e25564c8ba0342512cc91d4f49d71f56d
AlexKarv <alex.testengineer@gmail.com>
2023-09-01 10:33:46 +0300

    dddd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,12 +9,16 @@ public class Main {
         System.out.printf("Hello and welcome!");
         System.out.printf("Hello and welcome!");
 
+
+        System.out.println("test");
+
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
+
         }
     }
 }
